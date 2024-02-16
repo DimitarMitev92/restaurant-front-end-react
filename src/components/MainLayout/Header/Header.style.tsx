c73f345888a0e98510b18f3c60949f147943db50
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-16 15:25:29 +0200

    responsive header navbar with working links

---
+++ src/components/MainLayout/Header/Header.style.tsx
@@ -0,0 +1,134 @@
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import styled from "styled-components";
+
+export const Navbar = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--color-black);
+  padding: 1rem 2rem;
+
+  @media screen and (max-width: 1150px) {
+    .app__navbar-links {
+      display: none;
+    }
+
+    .app__navbar-smallscreen {
+      display: flex;
+    }
+  }
+
+  @media screen and (max-width: 650px) {
+    padding: 1rem;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const LogoImage = styled.img`
+  width: 150px;
+
+  @media screen and (min-width: 2000px) {
+    width: 210px;
+  }
+
+  @media screen and (max-width: 650px) {
+    width: 110px;
+  }
+`;
+
+export const LinksContainer = styled.ul`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+
+  @media screen and (max-width: 1150px) {
+    display: none;
+  }
+`;
+
+export const NavLi = styled.li`
+  margin: 0 1rem;
+  cursor: pointer;
+  color: var(--color-white);
+
+  &:hover {
+    color: var(--color-grey);
+  }
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const LoginLink = styled.a`
+  margin: 0 1rem;
+  text-decoration: none;
+  transition: 0.5s ease;
+  color: var(--color-white);
+
+  &:hover {
+    border-bottom: 1px solid var(--color-golden);
+  }
+`;
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 30px;
+  background: var(--color-grey);
+`;
+
+export const SmallScreenContainer = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1150px) {
+    display: flex;
+  }
+`;
+
+export const SmallScreenOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: var(--color-black);
+  transition: 0.5s ease;
+  flex-direction: column;
+  z-index: 5;
+`;
+
+export const CloseIcon = styled(MdOutlineRestaurantMenu)`
+  font-size: 27px;
+  color: var(--color-golden);
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+`;
+
+export const SmallScreenLinks = styled.ul`
+  list-style: none;
+`;
+
+export const SmallScreenNavLink = styled.li`
+  margin: 2rem;
+  cursor: pointer;
+  color: var(--color-golden);
+  font-size: 2rem;
+  text-align: center;
+  font-family: var(--font-base);
+
+  &:hover {
+    color: var(--color-white);
+  }
+`;
--- src/components/MainLayout/Header/Header.tsx
+++ src/components/MainLayout/Header/Header.tsx
@@ -1,5 +1,87 @@
-import React from "react";
+import { useState } from "react";
+import {
+  CloseIcon,
+  Divider,
+  LinksContainer,
+  LoginContainer,
+  LoginLink,
+  LogoContainer,
+  LogoImage,
+  NavLi,
+  Navbar,
+  SmallScreenContainer,
+  SmallScreenLinks,
+  SmallScreenNavLink,
+  SmallScreenOverlay,
+} from "./Header.style";
+import image from "../../../assets/gericht.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-  return <div>Header</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <Navbar>
+      <LogoContainer>
+        <LogoImage src={image} alt="app__logo" />
+      </LogoContainer>
+      <LinksContainer>
+        <NavLi className="p__opensans">
+          <NavLink to="/">Home</NavLink>
+        </NavLi>
+        <NavLi className="p__opensans">
+          <NavLink to="/#about">About Us</NavLink>
+        </NavLi>
+        <NavLi className="p__opensans">
+          <NavLink to="/#most-ordered">Most Ordered</NavLink>
+        </NavLi>
+        <NavLi className="p__opensans">
+          <NavLink to="/restaurants">Restaurants</NavLink>
+        </NavLi>
+        <NavLi className="p__opensans">
+          <NavLink to="/admin-dashboard">Admin Dashboard</NavLink>
+        </NavLi>
+      </LinksContainer>
+      <LoginContainer>
+        <LoginLink href="/auth" className="p__opensans">
+          Log In / Registration
+        </LoginLink>
+        <Divider />
+      </LoginContainer>
+      <SmallScreenContainer>
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <SmallScreenOverlay className="flex__center slide-bottom">
+            <CloseIcon
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <SmallScreenLinks>
+              <SmallScreenNavLink onClick={() => setToggleMenu(false)}>
+                <NavLink to="/">Home</NavLink>
+              </SmallScreenNavLink>
+              <SmallScreenNavLink onClick={() => setToggleMenu(false)}>
+                <NavLink to="/#about-us">About Us</NavLink>
+              </SmallScreenNavLink>
+              <SmallScreenNavLink onClick={() => setToggleMenu(false)}>
+                <NavLink to="/#most-ordered">Most Ordered</NavLink>
+              </SmallScreenNavLink>
+              <SmallScreenNavLink onClick={() => setToggleMenu(false)}>
+                <NavLink to="/restaurants">Restaurants</NavLink>
+              </SmallScreenNavLink>
+              <SmallScreenNavLink onClick={() => setToggleMenu(false)}>
+                <NavLink to="/admin-dashboard">Admin Dashboard</NavLink>
+              </SmallScreenNavLink>
+            </SmallScreenLinks>
+          </SmallScreenOverlay>
+        )}
+      </SmallScreenContainer>
+    </Navbar>
+  );
 };
