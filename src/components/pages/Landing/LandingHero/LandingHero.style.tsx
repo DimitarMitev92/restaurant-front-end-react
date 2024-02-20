c6cf27303780e5fd9a427718ea356ca33106d7ab
Radina Georgieva <radina940729@gmail.com>
2024-02-20 11:16:52 +0200

    feat(landingHero):add landing hero components with style and logic to display location options(reusable input), add location interface and style with basic media queries

---
+++ src/components/pages/Landing/LandingHero/LandingHero.style.tsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const HeroWrapper = styled.div`
+  background-color: var(--color-yellow);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 3rem 6rem;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    text-align: center; 
+    padding: 3rem 2rem; 
+  }
+`;
+
+export const WrapperInfo = styled.div`
+  flex: 1;
+  @media (max-width: 768px) {
+    order: 2; 
+  }
+`;
+
+export const HeroTitle = styled.h1`
+  color: var(--color-green);
+  letter-spacing: 0.04rem;
+  text-transform: uppercase;
+  line-height: 117px;
+  font-size: 90px;
+
+  @media (max-width: 768px) {
+    font-size: 60px; 
+    line-height: normal; 
+  }
+`;
+
+export const Description = styled.p`
+  margin: 2rem 0;
+  color: white;
+`;
+
+export const WrapperImg = styled.div`
+  flex: 1;
+  text-align: right;
+  img {
+    width: 80%;
+  }
+   @media (max-width: 768px) {
+    text-align: center; 
+    img {
+      width: 100%; 
+    }
+  }
+`;
--- src/components/pages/Landing/LandingHero/LandingHero.tsx
+++ src/components/pages/Landing/LandingHero/LandingHero.tsx
@@ -1,3 +1,40 @@
+import {
+  Description,
+  HeroTitle,
+  HeroWrapper,
+  WrapperImg,
+  WrapperInfo,
+} from "./LandingHero.style";
+import imageSrc from "../../../../images/logan-jeffrey-g5DNZ4X6RSc-unsplash.jpg";
+import UnifiedInput from "../../../ui-elements/input";
+import { PulseLoader } from "react-spinners";
+import { useLandingHeroLogic } from "./LandingHero.logic";
+
 export const LandingHero = () => {
-  return <div>LandingHero</div>;
+  const { loading, error, selectedLocation, handleChange, options } =
+    useLandingHeroLogic();
+
+  return (
+    <HeroWrapper>
+      <WrapperInfo>
+        <HeroTitle>Are you hungry?</HeroTitle>
+        <Description>Order something delicious</Description>
+        {loading && <PulseLoader color="#4caf50" size={5} />}
+        {error && <p>Error fetching locations.</p>}
+        {!loading && !error && (
+          <UnifiedInput
+            type="select"
+            onChange={handleChange}
+            value={selectedLocation}
+            placeholder="Select location"
+            name="location"
+            options={options || []}
+          />
+        )}
+      </WrapperInfo>
+      <WrapperImg>
+        <img src={imageSrc} alt="header img" />
+      </WrapperImg>
+    </HeroWrapper>
+  );
 };
