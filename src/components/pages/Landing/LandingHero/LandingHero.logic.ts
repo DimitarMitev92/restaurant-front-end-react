5998269a5721807d918caac7e9b118a4300c4446
Radina Georgieva <radina940729@gmail.com>
2024-02-26 11:45:53 +0200

    feat(landingHero):add logic if the user select location to render landing restaurants depending on the selection

--- src/components/pages/Landing/LandingHero/LandingHero.logic.ts
+++ src/components/pages/Landing/LandingHero/LandingHero.logic.ts
@@ -1,30 +1,56 @@
-import { useState, ChangeEvent } from "react";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import { useLocations } from "../../../../hooks/useLocations";
 import { Location } from "./LandingHero.static";
+import { useRestaurants } from "../../../../hooks/useRestaurants";
+import { useRestaurantsByLocationId } from "../../../../hooks/useRestaurantsByLocation";
+import { LocationData } from "../../../../static/interfaces";
 
-export const useLandingHeroLogic = () => {
-  const { locations, loading, error } = useLocations();
+export const useLandingHeroLogic = (
+  selectedLocation: string,
+  setSelectedLocation: Dispatch<SetStateAction<string>>
+) => {
+  const { locations } = useLocations();
 
-  const [selectedLocation, setSelectedLocation] = useState<string>("");
+  const {
+    restaurants: allRestaurants,
+    loading: loadingAll,
+    error: errorAll,
+  } = useRestaurants();
 
-  const handleChange = (
+  const {
+    restaurantsByLocation,
+    loading: loadingByLocation,
+    error: errorByLocation,
+  } = useRestaurantsByLocationId(selectedLocation);
+
+  const isLoading = selectedLocation ? loadingByLocation : loadingAll;
+  const error = selectedLocation ? errorByLocation : errorAll;
+  const restaurants = selectedLocation ? restaurantsByLocation : allRestaurants;
+
+  const options = locations?.map((location: Location) => ({
+    label: location.name,
+    value: location.id,
+  }));
+  const selectedLocationName = locations?.find(
+    (location: LocationData) => location.id === selectedLocation
+  )?.name;
+
+  const handleLocationChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
   ) => {
     const value = (e.target as HTMLSelectElement).value;
     setSelectedLocation(value);
   };
 
-  const options = locations?.map((location: Location) => ({
-    label: location.name,
-    value: location.name,
-  }));
   return {
     locations,
-    loading,
     error,
     selectedLocation,
     setSelectedLocation,
-    handleChange,
+    handleLocationChange,
+    selectedLocationName,
     options,
+    isLoading,
+    restaurants,
   };
 };
--- src/components/pages/Landing/LandingHero/LandingHero.static.ts
+++ src/components/pages/Landing/LandingHero/LandingHero.static.ts
@@ -1,4 +1,11 @@
+import { Dispatch, SetStateAction } from "react";
+
 export interface Location {
-    id:string;
-    name: string;
-  }
+  id: string;
+  name: string;
+}
+
+export interface LandingHeroProps {
+  selectedLocation: string;
+  setSelectedLocation: Dispatch<SetStateAction<string>>;
+}
