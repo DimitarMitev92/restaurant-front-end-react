88f45e06abd8a585e05c706f919b015b628a2dea
Radina Georgieva <radina940729@gmail.com>
2024-02-27 09:44:12 +0200

    feat(deleteMealPopUp,deleteMenuPopUp): add pop ups for delete menu and delete meal

---
+++ src/components/PopUp/DeleteMealPopUp.tsx
@@ -0,0 +1,26 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { CancelButtonPopup, PopUpContainer } from "./PopUp.style";
+import DeleteMealMessageForm from "../Forms/DeleteMealMessageForm/DeleteMealMessageForm";
+
+const DeleteMealPopUp = ({ onCancel, deleteMessage }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathSegments = location.pathname.split("/");
+  const deletedId = pathSegments[pathSegments.length - 1];
+
+  return (
+    <PopUpContainer>
+      <CancelButtonPopup
+        onClick={() => {
+          onCancel();
+          navigate(-1);
+        }}
+      >
+        X
+      </CancelButtonPopup>
+      <DeleteMealMessageForm deletedId={deletedId} />
+    </PopUpContainer>
+  );
+};
+
+export default DeleteMealPopUp;
