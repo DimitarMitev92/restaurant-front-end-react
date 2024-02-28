b85a2fd9e0eafbff52fa4f5db8db0889166d9b5c
Petar Georgiev <petiyo07@abv.bg>
2024-02-28 15:39:36 +0200

    fix(most-ordered-meal): fixed and added some styles

--- src/components/MostOrderedMeal/MostOrderedMeal.style.tsx
+++ src/components/MostOrderedMeal/MostOrderedMeal.style.tsx
@@ -16,3 +16,24 @@ export const Card = styled.div`
     box-shadow: 1px 1px 25px rgba(0, 0, 0, 0.1);
   }
 `;
+
+export const ImageContainer = styled.div`
+  padding: 1rem;
+  overflow: hidden;
+  height: 17rem;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+`;
+
+export const FoodImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 8px;
+  transition: filter 0.3s;
+`;
