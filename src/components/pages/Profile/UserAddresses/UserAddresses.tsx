a0fced11e920295eb7e1c66ff6b62c80e38e044f
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-29 16:34:13 +0200

    fix(user-addresses): export user addresses

--- src/components/pages/Profile/UserAddresses/UserAddresses.tsx
+++ src/components/pages/Profile/UserAddresses/UserAddresses.tsx
@@ -1,4 +1,3 @@
-import { useAuth } from "../../../../context/AuthProvider";
 import {
   AdminButtons,
   StyledRemoveButton,
@@ -9,48 +8,12 @@ import {
   AddressText,
   UserAddressesWrapper,
 } from "./UserAddresses.style";
-import { addressService } from "../../../../services/addressService";
-import { useEffect, useState } from "react";
 import EmptyList from "../../../EmptyList/EmptyList";
 import PulseLoader from "react-spinners/PulseLoader";
+import { useUserAddresses } from "./UserAddresses.logic";
 
 export const UserAddresses = () => {
-  const { user } = useAuth();
-  const userId = user?.user.id || "";
-
-  const [addresses, setAddresses] = useState<AddressDataApi[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [triggerDelete, setTriggerDelete] = useState(false);
-
-  useEffect(() => {
-    const fetchAddressesByUserId = async () => {
-      try {
-        setIsLoading(true);
-        const addresses = await addressService.fetchAddressesByUserId(userId);
-        setAddresses(addresses);
-      } catch (error) {
-        setAddresses([]);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchAddressesByUserId();
-  }, [userId, triggerDelete]);
-
-  const handleDeleteAddress = async (addressId: string) => {
-    try {
-      const isConfirmed = window.confirm(
-        "Are you sure you want to delete this address?"
-      );
-      if (isConfirmed) {
-        const data = await addressService.deleteSoft(addressId);
-        setTriggerDelete((prev) => !prev);
-      }
-    } catch (error) {
-      console.error("Error deleting address:", error);
-    }
-  };
+  const { addresses, isLoading, handleDeleteAddress } = useUserAddresses();
 
   return (
     <UserAddressesWrapper>
