c70c443c788cb9920d8712b6c61a7bb9b38b3b23
Dimitar Mitev <dimitar.mitev@yara.com>
2024-02-29 16:22:23 +0200

    fix(location): export location logic

--- src/components/pages/AdminDashboard/DisplayAndDelete/Locations/Locations.tsx
+++ src/components/pages/AdminDashboard/DisplayAndDelete/Locations/Locations.tsx
@@ -2,9 +2,7 @@ import {
   AdminButtons,
   StyledRemoveButton,
 } from "../../../../MealHolder/Meal/Meal.style";
-import { useEffect, useState } from "react";
 import EmptyList from "../../../../EmptyList/EmptyList";
-import { locationService } from "../../../../../services/locationService";
 import {
   AddressCard,
   AddressText,
@@ -13,42 +11,11 @@ import {
 import { LocationDataApi } from "../../../../../static/interfaces";
 import { LocationsProps } from "../../AdminDashboard.static";
 import { PulseLoader } from "react-spinners";
+import { useLocationLogic } from "./Location.logic";
 
 export const Locations: React.FC<LocationsProps> = ({ onDelete }) => {
-  const [locations, setLocations] = useState<LocationDataApi[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [triggerDelete, setTriggerDelete] = useState(false);
-
-  useEffect(() => {
-    const fetchLocations = async () => {
-      try {
-        setIsLoading(true);
-        const locations = await locationService.fetchLocations();
-        setLocations(locations);
-      } catch (error) {
-        setLocations([]);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchLocations();
-  }, [triggerDelete]);
-
-  const handleDeleteLocation = async (locationId: string) => {
-    try {
-      const isConfirmed = window.confirm(
-        "Are you sure you want to delete this location?"
-      );
-
-      if (isConfirmed) {
-        await locationService.deleteLocationById(locationId);
-        setTriggerDelete((prev) => !prev);
-        onDelete(locationId);
-      }
-    } catch (error) {
-      console.error("Error deleting location:", error);
-    }
-  };
+  const { locations, isLoading, handleDeleteLocation } =
+    useLocationLogic(onDelete);
 
   if (isLoading) {
     return <PulseLoader color="var(--color-green)" size={5} />;
